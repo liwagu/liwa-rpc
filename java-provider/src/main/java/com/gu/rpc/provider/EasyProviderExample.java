f39f0dfe8e7da5e52aea98398dcdff661ef50ed5
liwa <guliwa@foxmail.com>
2024-03-05 17:07:05 +0800

    feat: provider注册服务

--- java-provider/src/main/java/com/gu/rpc/provider/EasyProviderExample.java
+++ java-provider/src/main/java/com/gu/rpc/provider/EasyProviderExample.java
@@ -1,9 +1,13 @@
 package com.gu.rpc.provider;
 
+import com.gu.rpc.common.service.UserService;
+import com.gu.rpc.registry.LocalRegistry;
 import com.gu.rpc.server.VertxHttpServer;
 
 public class EasyProviderExample {
     public static void main(String[] args) {
+        //注册服务
+        LocalRegistry.register(UserService.class.getName(), UserServiceImpl.class);
         // 启动web服务
         VertxHttpServer vertxHttpServer = new VertxHttpServer();
         vertxHttpServer.doStartServer(8080);
