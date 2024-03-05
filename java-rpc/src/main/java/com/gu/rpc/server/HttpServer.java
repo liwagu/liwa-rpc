065a15f00f60d523e744bbb6b1f0bf3d445e411e
liwa <guliwa@foxmail.com>
2024-03-05 16:12:24 +0800

    feat: java-rpc

---
+++ java-rpc/src/main/java/com/gu/rpc/server/HttpServer.java
@@ -0,0 +1,5 @@
+package com.gu.rpc.server;
+
+public interface HttpServer {
+    void doStartServer(int port);
+}
---
+++ java-rpc/src/main/java/com/gu/rpc/server/VertxHttpServer.java
@@ -0,0 +1,33 @@
+package com.gu.rpc.server;
+
+import io.vertx.core.Vertx;
+
+public class VertxHttpServer implements HttpServer {
+    @Override
+    public void doStartServer(int port) {
+        Vertx vertx = Vertx.vertx();//创建Vertx.x实例
+
+        //创建HTTP服务器
+        io.vertx.core.http.HttpServer httpServer = vertx.createHttpServer();
+        //监听端口并处理请求
+        httpServer.requestHandler(request -> {
+            // 处理http请求
+            System.out.println("Received request: " + request.method() + " " + request.uri());
+
+            // 发送http响应
+            request.response()
+                    .putHeader("content-type", "text/plain")
+                    .end("Hello from Vert.x HTTP server");
+        });
+
+        // 启动 HTTP 服务并监听指定端口
+        httpServer.listen(port, httpServerAsyncResult -> {
+            if (httpServerAsyncResult.succeeded()) {
+                System.out.println("Server is mow listening on port" + port);
+            } else {
+                System.err.println("Failed to start server: " + httpServerAsyncResult.cause());
+            }
+        });
+
+    }
+}
