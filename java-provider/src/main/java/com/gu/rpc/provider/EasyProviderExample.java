4dce3d360da7dd7b2c07ffa8e71b4cc830188364
liwa <guliwa@foxmail.com>
2024-03-05 16:15:05 +0800

    feat: provider启动服务

--- java-provider/src/main/java/com/gu/rpc/provider/EasyProviderExample.java
+++ java-provider/src/main/java/com/gu/rpc/provider/EasyProviderExample.java
@@ -1,7 +1,11 @@
 package com.gu.rpc.provider;
 
+import com.gu.rpc.server.VertxHttpServer;
+
 public class EasyProviderExample {
     public static void main(String[] args) {
-        // 提供服务
+        // 启动web服务
+        VertxHttpServer vertxHttpServer = new VertxHttpServer();
+        vertxHttpServer.doStartServer(8080);
     }
 }
