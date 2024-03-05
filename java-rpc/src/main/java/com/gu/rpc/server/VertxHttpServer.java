598ee83fcaefc9c304f780e7e553f67b34c30c5d
liwa <guliwa@foxmail.com>
2024-03-05 19:35:12 +0800

    feat: java-rpc: VertxHttpServer请求替代

--- java-rpc/src/main/java/com/gu/rpc/server/VertxHttpServer.java
+++ java-rpc/src/main/java/com/gu/rpc/server/VertxHttpServer.java
@@ -1,6 +1,11 @@
 package com.gu.rpc.server;
 
 import io.vertx.core.Vertx;
+/**
+ * @description Vertx HTTP 服务器
+ * @author liwa
+ * @date 2024-03-05 19:29
+ */
 
 public class VertxHttpServer implements HttpServer {
     @Override
@@ -10,7 +15,8 @@ public class VertxHttpServer implements HttpServer {
         //创建HTTP服务器
         io.vertx.core.http.HttpServer httpServer = vertx.createHttpServer();
         //监听端口并处理请求
-        httpServer.requestHandler(request -> {
+        httpServer.requestHandler(new HttpServerHandler()); // 用HttpServerHandler 代替了原来的手动处理
+       /* httpServer.requestHandler(request -> {
             // 处理http请求
             System.out.println("Received request: " + request.method() + " " + request.uri());
 
@@ -18,7 +24,7 @@ public class VertxHttpServer implements HttpServer {
             request.response()
                     .putHeader("content-type", "text/plain")
                     .end("Hello from Vert.x HTTP server");
-        });
+        });*/
 
         // 启动 HTTP 服务并监听指定端口
         httpServer.listen(port, httpServerAsyncResult -> {
